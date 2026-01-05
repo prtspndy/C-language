310a877653e872e94d05fa587ddf077e380da35e
Pritesh Pandya <prtspndy@gmail.com>
2026-01-05 21:59:03 +0530

    Add files via upload

---
+++ C language/Lab-2/if-else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 10;
+
+    if (n > 5)
+    {
+        printf("%d is greater than 5", n);
+    }
+    else
+    {
+        printf("%d is less than 5", n);
+    }
+
+    return 0;
+}
